047d6005005d83385a18e2e68a8f7f053c180aec
Russell Toris <rctoris@wpu.edu>
2012-10-11 12:50:26 -0400

    'change' emitted when a user switches camera feeds -- useful for logging

--- mjpegcanvas.js
+++ mjpegcanvas.js
@@ -34,7 +34,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *
  *   Author: Russell Toris
- *  Version: September 27, 2012
+ *  Version: October 9, 2012
  *
  *********************************************************************/
 
@@ -52,29 +52,29 @@ var MjpegCanvas = function(options) {
   mjpegCanvas.height = options.height;
 
   // current streaming topic
-  var _stream = null;
-  var _img = null;
+  var curStream = null;
+  var img = null;
 
   // grab the canvas
-  var _canvas = document.getElementById(mjpegCanvas.canvasID);
-  var _context = _canvas.getContext('2d');
+  var canvas = document.getElementById(mjpegCanvas.canvasID);
+  var context = canvas.getContext('2d');
 
-  var _createImage = function() {
+  var createImage = function() {
     // create the image to hold the stream
-    _img = new Image();
-    var _src = 'http://' + mjpegCanvas.host + ':' + mjpegCanvas.port
-        + '/stream?topic=' + _stream;
+    img = new Image();
+    var src = 'http://' + mjpegCanvas.host + ':' + mjpegCanvas.port
+        + '/stream?topic=' + curStream;
     // check for various options
     if (mjpegCanvas.width > 0) {
-      _src += '?width=' + mjpegCanvas.width;
+      src += '?width=' + mjpegCanvas.width;
     }
     if (mjpegCanvas.width > 0) {
-      _src += '?height=' + mjpegCanvas.height;
+      src += '?height=' + mjpegCanvas.height;
     }
     if (mjpegCanvas.quality > 0) {
-      _src += '?quality=' + mjpegCanvas.quality;
+      src += '?quality=' + mjpegCanvas.quality;
     }
-    _img.src = _src;
+    img.src = src;
   };
 
   // check if the topics is a list or single topic
@@ -85,35 +85,35 @@ var MjpegCanvas = function(options) {
     }
 
     // start the stream with the first topic
-    _stream = mjpegCanvas.topic[mjpegCanvas.defaultStream];
+    curStream = mjpegCanvas.topic[mjpegCanvas.defaultStream];
 
     // button options
-    _context.globalAlpha = 0.66;
-    var _buttonMargin = 10;
-    var _buttonPadding = 5;
-    var _buttonHeight = 25;
-    var _buttonWidth = 55;
-    var _buttonColor = 'black';
-    var _buttonStrokeSize = 5;
-    var _buttonStrokeColor = 'blue';
-    var _editFont = '16pt Verdana';
-    var _editColor = 'white';
+    context.globalAlpha = 0.66;
+    var buttonMargin = 10;
+    var buttonPadding = 5;
+    var buttonHeight = 25;
+    var buttonWidth = 55;
+    var buttonColor = 'black';
+    var buttonStrokeSize = 5;
+    var buttonStrokeColor = 'blue';
+    var editFont = '16pt Verdana';
+    var editColor = 'white';
 
     // menu div
-    var _menu = document.createElement('div');
-    document.getElementsByTagName('body')[0].appendChild(_menu);
-    _menu.style.display = 'none';
+    var menu = document.createElement('div');
+    document.getElementsByTagName('body')[0].appendChild(menu);
+    menu.style.display = 'none';
 
     // create the mouseovers
-    var _mouseEnter = false;
-    var _menuOpen = false;
-    _canvas.addEventListener('mouseenter', function(e) {
-      _mouseEnter = true;
+    var mouseEnter = false;
+    var menuOpen = false;
+    canvas.addEventListener('mouseenter', function(e) {
+      mouseEnter = true;
     }, false);
-    _canvas.addEventListener('mouseleave', function(e) {
-      _mouseEnter = false;
+    canvas.addEventListener('mouseleave', function(e) {
+      mouseEnter = false;
     }, false);
-    _canvas
+    canvas
         .addEventListener(
             'click',
             function(e) {
@@ -122,7 +122,7 @@ var MjpegCanvas = function(options) {
 
               var offsetLeft = 0;
               var offsetTop = 0;
-              var element = _canvas;
+              var element = canvas;
               while (element && !isNaN(element.offsetLeft)
                   && !isNaN(element.offsetTop)) {
                 offsetLeft += element.offsetLeft - element.scrollLeft;
@@ -132,14 +132,14 @@ var MjpegCanvas = function(options) {
 
               var x = e.pageX - offsetLeft;
               var y = e.pageY - offsetTop;
-              var height = _canvas.getAttribute('height');
-              var width = _canvas.getAttribute('width');
+              var height = canvas.getAttribute('height');
+              var width = canvas.getAttribute('width');
 
               // check if we are in the 'edit' button
-              if (x < (_buttonWidth + _buttonMargin) && x > _buttonMargin
-                  && y > (height - (_buttonHeight + _buttonMargin))
-                  && y < height - _buttonMargin) {
-                _menuOpen = true;
+              if (x < (buttonWidth + buttonMargin) && x > buttonMargin
+                  && y > (height - (buttonHeight + buttonMargin))
+                  && y < height - buttonMargin) {
+                menuOpen = true;
 
                 // create the menu
                 var html = '<h2>Camera Streams</h2><hr /><br /><form><label for="stream">Stream</label><select name="stream" id="stream-'
@@ -147,7 +147,7 @@ var MjpegCanvas = function(options) {
                 for ( var i = 0; i < mjpegCanvas.topic.length; i++) {
                   // check if this is the selected option
                   var selected = '';
-                  if (mjpegCanvas.topic[i] === _stream) {
+                  if (mjpegCanvas.topic[i] === curStream) {
                     var selected = 'selected="selected"';
                   }
                   html += '<option value="' + mjpegCanvas.topic[i] + '" '
@@ -164,23 +164,24 @@ var MjpegCanvas = function(options) {
                     + '">Close</button>';
 
                 // display the menu
-                _menu.innerHTML = html;
-                _menu.style.position = 'absolute';
-                _menu.style.top = offsetTop + 'px';
-                _menu.style.left = offsetLeft + 'px';
-                _menu.style.width = width + 'px';
-                _menu.style.display = 'block';
+                menu.innerHTML = html;
+                menu.style.position = 'absolute';
+                menu.style.top = offsetTop + 'px';
+                menu.style.left = offsetLeft + 'px';
+                menu.style.width = width + 'px';
+                menu.style.display = 'block';
 
                 // dropdown change listener
                 var select = document.getElementById('stream-' + id);
                 select.addEventListener('click', function() {
                   var stream = select.options[select.selectedIndex].value;
-                  // make sure it is a new
-                  // stream
-                  if (stream !== _stream) {
-                    _stream = stream;
-                    _img = null;
-                    _createImage();
+                  // make sure it is a new stream
+                  if (stream !== curStream) {
+                    curStream = stream;
+                    img = null;
+                    createImage();
+                    // emit an event for the change
+                    mjpegCanvas.emit('change', stream);
                   }
                 }, false);
 
@@ -188,63 +189,64 @@ var MjpegCanvas = function(options) {
                 var button = document.getElementById('button-' + id);
                 button.addEventListener('click', function(e) {
                   // close the menu
-                  _menuOpen = false;
-                  _menu.style.display = 'none';
+                  menuOpen = false;
+                  menu.style.display = 'none';
                 }, false);
               }
             }, false);
   } else {
-    _stream = mjpegCanvas.topic;
+    curStream = mjpegCanvas.topic;
   }
 
   // a function to draw the image onto the canvas
-  function draw(_img) {
+  function draw(img) {
     // grab the current sizes of the canvas
-    var width = _canvas.getAttribute('width');
-    var height = _canvas.getAttribute('height');
+    var width = canvas.getAttribute('width');
+    var height = canvas.getAttribute('height');
 
     // grab the drawing context and draw the image
-    if (_img) {
-      _context.drawImage(_img, 0, 0, width, height);
+    if (img) {
+      context.drawImage(img, 0, 0, width, height);
     } else {
-      _context.clearRect(0, 0, width, height);
+      context.clearRect(0, 0, width, height);
     }
 
-    if (_mouseEnter && !_menuOpen) {
+    if (mouseEnter && !menuOpen) {
       // create the "swap" button
-      _context.globalAlpha = 0.66;
-      _context.beginPath();
-      _context.rect(_buttonMargin, height - (_buttonHeight + _buttonMargin),
-          _buttonWidth, _buttonHeight);
-      _context.fillStyle = _buttonColor;
-      _context.fill();
-      _context.lineWidth = _buttonStrokeSize;
-      _context.strokeStyle = _buttonStrokeColor;
-      _context.stroke();
+      context.globalAlpha = 0.66;
+      context.beginPath();
+      context.rect(buttonMargin, height - (buttonHeight + buttonMargin),
+          buttonWidth, buttonHeight);
+      context.fillStyle = buttonColor;
+      context.fill();
+      context.lineWidth = buttonStrokeSize;
+      context.strokeStyle = buttonStrokeColor;
+      context.stroke();
 
       // draw the text on the button
-      _context.font = _editFont;
-      _context.fillStyle = _editColor;
-      _context.fillText('Edit', _buttonMargin + _buttonPadding, height
-          - (_buttonMargin + _buttonPadding));
+      context.font = editFont;
+      context.fillStyle = editColor;
+      context.fillText('Edit', buttonMargin + buttonPadding, height
+          - (buttonMargin + buttonPadding));
     }
 
-    if (_menuOpen) {
+    if (menuOpen) {
       // create the white box
-      _context.globalAlpha = 0.66;
-      _context.beginPath();
-      _context.rect(0, 0, width, height);
-      _context.fillStyle = 'white';
-      _context.fill();
+      context.globalAlpha = 0.66;
+      context.beginPath();
+      context.rect(0, 0, width, height);
+      context.fillStyle = 'white';
+      context.fill();
     }
-    _context.globalAlpha = 1;
+    context.globalAlpha = 1;
   }
 
   // grab the initial stream
-  _createImage();
+  createImage();
 
   // redraw the image every 100 ms
   setInterval(function() {
-    draw(_img);
+    draw(img);
   }, 100);
 };
+MjpegCanvas.prototype.__proto__ = EventEmitter2.prototype;
